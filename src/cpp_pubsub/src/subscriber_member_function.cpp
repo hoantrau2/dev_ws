3e5d9c6c78e2cb0ba8316404bbb5d51eca8a6f8a
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-11-19 23:05:18 +0700

    msg and srv

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -27,7 +27,7 @@ public:
   : Node("minimal_subscriber")
   {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic11", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
 private:
